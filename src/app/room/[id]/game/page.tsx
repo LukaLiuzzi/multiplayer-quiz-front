c09a6bf0077cd53f64f26b7663c474712ba07152
Luka <lukaliuzzidev@gmail.com>
2023-12-09 02:54:22 -0300

    add answer questions component

--- src/app/room/[id]/game/page.tsx
+++ src/app/room/[id]/game/page.tsx
@@ -2,11 +2,17 @@
 
 import { useSocketContext } from "@/context/SocketContext"
 import CreateQuestions from "@/components/CreateQuestions/CreateQuestions"
+import AnswerQuestions from "@/components/AnswerQuestions/AnswerQuestions"
 
 const GamePage: React.FC = () => {
   const { room } = useSocketContext()
   console.log("room", room)
-  return <>{room?.state === "question" && <CreateQuestions />}</>
+  return (
+    <>
+      {room?.state === "question" && <CreateQuestions />}
+      {room?.state === "answer" && <AnswerQuestions />}
+    </>
+  )
 }
 
 export default GamePage
---
+++ src/components/AnswerQuestions/AnswerQuestions.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { useSocketContext } from "@/context/SocketContext"
+import { Answer, Question } from "@/types"
+import { useState, useEffect } from "react"
+import { toast } from "react-toastify"
+
+const AnswerQuestions: React.FC = () => {
+  const { room, socket } = useSocketContext()
+  const [questions, setQuestions] = useState<Question[]>([])
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<number | null>(
+    null
+  )
+  const [correctAnswerIndex, setCorrectAnswerIndex] = useState<number | null>(
+    null
+  )
+
+  const mixAnswers = (answers: Answer[]) => {
+    return answers.sort(() => Math.random() - 0.5)
+  }
+
+  const handleSetQuestions = () => {
+    const rival = room?.players.find((player) => player.id !== socket?.id)
+
+    if (rival?.questions) {
+      rival.questions.forEach((question) => {
+        question.answers = mixAnswers(question.answers)
+      })
+      setQuestions(rival.questions)
+    }
+  }
+
+  useEffect(() => {
+    handleSetQuestions()
+  }, [])
+
+  useEffect(() => {
+    // Reset selected answer when moving to the next question
+    setSelectedAnswerIndex(null)
+    setCorrectAnswerIndex(null)
+  }, [currentQuestionIndex])
+
+  const handleAnswerClick = (answerIndex: number) => {
+    if (selectedAnswerIndex === null) {
+      setSelectedAnswerIndex(answerIndex)
+
+      // Show notification based on correctness
+      if (questions[currentQuestionIndex].answers[answerIndex].isCorrect) {
+        toast.success("Respuesta correcta!")
+      } else {
+        toast.error("Respuesta incorrecta! La respuesta correcta fue marcada.")
+        setCorrectAnswerIndex(
+          questions[currentQuestionIndex].answers.findIndex(
+            (answer) => answer.isCorrect
+          )
+        )
+      }
+    }
+  }
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1)
+    } else {
+      toast.info("You have completed all questions.")
+    }
+  }
+
+  return (
+    <div className="max-w-md mx-auto mt-8 p-4 border rounded shadow-md">
+      {currentQuestionIndex < questions.length && (
+        <div>
+          <h2 className="text-lg font-semibold mb-4">
+            Pregunta {currentQuestionIndex + 1}:
+          </h2>
+          <p className="mb-4">{questions[currentQuestionIndex].question}</p>
+
+          <div className="mb-4">
+            {questions[currentQuestionIndex].answers.map(
+              (answer, answerIndex) => (
+                <div
+                  key={answerIndex}
+                  className={`p-2 border rounded cursor-pointer mb-2 ${
+                    selectedAnswerIndex === answerIndex
+                      ? answer.isCorrect
+                        ? "bg-green-300"
+                        : "bg-red-300"
+                      : correctAnswerIndex === answerIndex
+                      ? "bg-green-300"
+                      : ""
+                  }`}
+                  onClick={() => handleAnswerClick(answerIndex)}
+                >
+                  {answer.text}
+                </div>
+              )
+            )}
+          </div>
+
+          <button
+            className={`bg-blue-500 text-white px-4 py-2 rounded w-full ${
+              selectedAnswerIndex === null ? "opacity-50" : ""
+            }`}
+            onClick={handleNextQuestion}
+            disabled={selectedAnswerIndex === null}
+          >
+            Siguiente Pregunta
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnswerQuestions
