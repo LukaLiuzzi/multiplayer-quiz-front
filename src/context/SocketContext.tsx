bb15fb972c11208c6e2a991805b7e1bf167872cf
lukaliuzzi <lukaliuzzidev@gmail.com>
2025-01-11 17:49:32 -0300

    remove polling transport from socket connection configuration

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -43,7 +43,7 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
     const socket = io(process.env.NEXT_PUBLIC_API_URL, {
       path: "/multiplayer-quiz-back/socket.io",
       withCredentials: true, // Si es necesario enviar cookies o credenciales
-      transports: ["polling", "websocket"],
+      transports: ["websocket"],
     })
 
     socket.on("connect", () => {
