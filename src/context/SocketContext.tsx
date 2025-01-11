83f941aafc4ff04e05c5255790dba316cd321b6a
lukaliuzzi <lukaliuzzidev@gmail.com>
2025-01-11 17:32:59 -0300

    add websocket transport option to socket connection configuration

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -43,6 +43,7 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
     const socket = io(process.env.NEXT_PUBLIC_API_URL, {
       path: "/multiplayer-quiz-back/socket.io",
       withCredentials: true, // Si es necesario enviar cookies o credenciales
+      transports: ["websocket"],
     })
 
     socket.on("connect", () => {
